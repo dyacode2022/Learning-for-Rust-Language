d41350a088b768886a277a6c4f286b2d6276ac44
dia7691 <51194584+dia-7691@users.noreply.github.com>
2020-07-17 22:54:36 +0900

    Var
    Var

---
+++ var/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    const MAX_POINTS: u32 = 100000;
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    let mut y = 3;
+    y = y + 5;
+
+    println!("var X value = {}", x);
+    println!("var Y value = {}", y);
+    println!("const MAX_POINTS is = {}", MAX_POINTS);
+}
