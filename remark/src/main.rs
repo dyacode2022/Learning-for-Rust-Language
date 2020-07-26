72fa21a13a5fe09adbc9750e46486c88497edff9
dia7691 <51194584+dia-7691@users.noreply.github.com>
2020-07-26 13:33:54 +0900

    Remark
    Remark

---
+++ remark/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("It is a Remark Source!"); //this is the remark!
+    //remark//remark//remark//remark//remark//remark//remark//remark
+    //remark//remark//remark
+    //remark//remark//remark//remark//remark//remark//remark//remark//remark//remark//remark
+    //remark
+    //remark
+    //remark
+    //remark
+    //remark
+    //cool
+    println!("Remark is not print!");
+}
